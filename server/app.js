5167ea0eece8b34348cf4d4eadad84f002f43069
Philip Hamner <philiphamner@gmail.com>
2020-05-02 15:55:25 -0700

    minor tweak to start and webpack commands in package.json.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1234;
+const port = 3333;
 const path = require('path');
 const { getAll } = require('../database/index.js')
 
