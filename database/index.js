7f2d361399562b6294e9b7369d097be7b4584c42
Philip Hamner <philiphamner@gmail.com>
2020-05-01 20:34:25 -0700

    Rearanged files and added script so as to better seed DB w/ npm script

--- database/index.js
+++ database/index.js
@@ -25,49 +25,50 @@ let carouselSchema = new mongoose.Schema({
 
 let Carousel = mongoose.model('Carousel', carouselSchema)
 
-const save = function() {
+module.exports = {
+  save: function() {
 
-  let newItem = {
-    id: 4,
-    itemName: 'Nikeyyyyyyyy',
-    type: 'shoes for yo mommyya',
-    colors: {
-      color1:['hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello'],
-      color2:['there', 'there', 'there', 'there', 'there', 'there', 'there', 'there'],
-      color3:['world', 'world', 'world', 'world', 'world', 'world', 'world', 'world']
+    let newItem = {
+      id: 4,
+      itemName: 'Nikeyyyyyyyy',
+      type: 'shoes for yo mommyya',
+      colors: {
+        color1:['hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello'],
+        color2:['there', 'there', 'there', 'there', 'there', 'there', 'there', 'there'],
+        color3:['world', 'world', 'world', 'world', 'world', 'world', 'world', 'world']
+      }
     }
+
+    let item = new Carousel(newItem)
+
+    item.save(function (err, item) {
+      if (err){
+        console.error(err);
+      } else {
+        console.log('SUCCESS: ', item)
+      }
+      // fluffy.speak();
+    });
+
+
+
+    //   // versionsAvailable: {a: 'hello', b: 'world'}
+    //   //   1: ['https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1000,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/Allbirds_W_Wool_Runner_Kotare_GREY_ANGLE_0f3bfe63-ac7d-4106-9acf-d26f8414ac53.png?v=1571655724', 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1000,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/Allbirds_W_Wool_Runner_Kotare_GREY_ANGLE_0f3bfe63-ac7d-4106-9acf-d26f8414ac53.png?v=1571655724']
+    //   // }
+    // }
+
+    // Carousel.insertOne(newItem)
+    // .then(result => {
+    //   console.log(`Successfully inserted ${result.insertedIds.length} items!`);
+    //   return result
+    // })
+    // .catch(err => console.error(`Failed to insert documents: ${err}`))
   }
-
-  let item = new Carousel(newItem)
-
-  item.save(function (err, item) {
-    if (err){
-      console.error(err);
-    } else {
-      console.log('SUCCESS: ', item)
-    }
-    // fluffy.speak();
-  });
-
-
-
-  //   // versionsAvailable: {a: 'hello', b: 'world'}
-  //   //   1: ['https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1000,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/Allbirds_W_Wool_Runner_Kotare_GREY_ANGLE_0f3bfe63-ac7d-4106-9acf-d26f8414ac53.png?v=1571655724', 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_1000,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/Allbirds_W_Wool_Runner_Kotare_GREY_ANGLE_0f3bfe63-ac7d-4106-9acf-d26f8414ac53.png?v=1571655724']
-  //   // }
-  // }
-
-  // Carousel.insertOne(newItem)
-  // .then(result => {
-  //   console.log(`Successfully inserted ${result.insertedIds.length} items!`);
-  //   return result
-  // })
-  // .catch(err => console.error(`Failed to insert documents: ${err}`))
+  
 }
 
-save()
 
-// {
-//     id: 1,
-//     itemName: 'Nike',
-//     type: 'shoe'
-// }
+// save()
+
+
+
---
+++ database/seedDB.js
@@ -0,0 +1,5 @@
+// var save = { save };
+const { save } = require('./index.js');
+
+save();
+//now do this 100x, and add data from S3 bucket.
