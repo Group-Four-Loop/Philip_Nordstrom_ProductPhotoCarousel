71bba0d46d8f5c1378c02baa176d1d671d802916
Philip Hamner <philiphamner@gmail.com>
2020-05-04 14:23:40 -0700

    Add buttons and functionality to allow user to move carousel forward and backwards.

---
+++ src/components/MoveCarouselForward.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var MoveCarouselForward = function(props){
+  return(
+    <button onClick={props.moveForward}>
+    Move Forwards
+    </button>
+  )
+  // console.log('hello world')
+}
+
+export default MoveCarouselForward;
